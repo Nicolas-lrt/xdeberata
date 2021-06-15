bdbc17cc6a0a606b11e1d27fd82ea19511cb6a7a
Nicolas Laurent <nicolas.laurent@universite-paris-saclay.fr>
2021-06-15 17:38:45 +0200

    début de la boutique 2

--- shopping/views.py
+++ shopping/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from account.models import Account, CartLine
+from shopping.models import Product
 
 
 def home_shop(request):
-    return render(request, 'shopping/home-shop.html')
+    produits = Product.objects.all()
+    qtyTotal = 0
+    client = Account.objects.filter(userId=request.user.id)
+    if CartLine.objects.filter(client__in=client):
+        cart = CartLine.objects.filter(client__in=client)
+        for cart_line in cart:
+            qtyTotal += cart_line.quantity
+
+    context = {'products': produits, 'cartQty': qtyTotal}
+
+    return render(request, 'shopping/home-shop.html', context)
